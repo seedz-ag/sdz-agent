da59306fc12bd305e6e55b6514f46504f5f73703
Diogo <diogo.nunes>
2021-10-15 10:25:45 -0300

    3 types os scopes available

--- src/config/scope.ts
+++ src/config/scope.ts
@@ -4,81 +4,101 @@ import ConfigScope from "sdz-agent-types/types/config.scope.type";
 
 const { MultiSelect, Select } = require("enquirer");
 
-export default async (config: ConfigScope | undefined) => {
-  const choices = [
-    {
-      name: "Clients",
-      file: "cliente.csv",
-    },
-    {
-      name: "Address",
-      file: "endereco.csv",
-    },
-    {
-      name: "Properties",
-      file: "propriedade.csv",
-    },
-    {
-      name: "Items",
-      file: "item.csv",
-    },
-    {
-      name: "ItemsBranding",
-      file: "item_branding.csv",
-    },
-    {
-      name: "ItemsGroup",
-      file: "item_grupo.csv",
-    },
-    {
-      name: "Orders",
-      file: "pedido.csv",
-    },
-    {
-      name: "OrdersItem",
-      file: "pedido_item.csv",
-    },
-    {
-      name: "Invoices",
-      file: "faturamento.csv",
-    },
-    {
-      name: "InvoicesItem",
-      file: "faturamento_item.csv",
-    },
-    {
-      name: "PaymentsSpecie",
-      file: "especie_pagamento.csv",
-    },
-    {
-      name: "PaymentsCondition",
-      file: "condicao_pagamento.csv",
-    },
-    {
-      name: "Providers",
-      file: "fornecedor.csv",
-    },
-    {
-      name: "AccountsPay",
-      file: "contas_pagar.csv",
-    },
-    {
-      name: "AccountsReceivable",
-      file: "contas_receber.csv",
-    },
-    {
-      name: "Vendors",
-      file: "vendedor.csv",
-    },
-    {
-      name: "Employees",
-      file: "funcionario.csv",
-    },
-    {
-      name: "Inventories",
-      file: "estoque.csv",
-    },
-  ];
+export default async (config: ConfigScope | undefined): Promise<any> => {
+  const choices = {
+    maximum: [
+      {
+        name: "Clients",
+        file: "cliente.csv",
+      },
+      {
+        name: "Address",
+        file: "endereco.csv",
+      },
+      {
+        name: "Properties",
+        file: "propriedade.csv",
+      },
+      {
+        name: "Items",
+        file: "item.csv",
+      },
+      {
+        name: "ItemsBranding",
+        file: "item_branding.csv",
+      },
+      {
+        name: "ItemsGroup",
+        file: "item_grupo.csv",
+      },
+      {
+        name: "Orders",
+        file: "pedido.csv",
+      },
+      {
+        name: "OrdersItem",
+        file: "pedido_item.csv",
+      },
+      {
+        name: "Invoices",
+        file: "faturamento.csv",
+      },
+      {
+        name: "InvoicesItem",
+        file: "faturamento_item.csv",
+      },
+      {
+        name: "PaymentsSpecie",
+        file: "especie_pagamento.csv",
+      },
+      {
+        name: "PaymentsCondition",
+        file: "condicao_pagamento.csv",
+      },
+      {
+        name: "Providers",
+        file: "fornecedor.csv",
+      },
+      {
+        name: "AccountsPay",
+        file: "contas_pagar.csv",
+      },
+      {
+        name: "AccountsReceivable",
+        file: "contas_receber.csv",
+      },
+      {
+        name: "Vendors",
+        file: "vendedor.csv",
+      },
+      {
+        name: "Employees",
+        file: "funcionario.csv",
+      },
+      {
+        name: "Inventories",
+        file: "estoque.csv",
+      },
+    ],
+    minimum: [
+      {
+        name: "Clients",
+        file: "cliente.csv",
+      },
+      {
+        name: "Invoices",
+        file: "faturamento.csv",
+      },
+      {
+        name: "InvoicesItem",
+        file: "faturamento_item.csv",
+      },
+      {
+        name: "AccountsReceivable",
+        file: "contas_receber.csv",
+      },
+    ],
+  };
 
   const prompt1 = new Select({
     name: "response",
@@ -87,26 +107,35 @@ export default async (config: ConfigScope | undefined) => {
     choices: ["minimum", "maximum", "custom"],
   });
 
-  const scopeType = await prompt1.run();
+  let scope: string[];
+  let type: string = await prompt1.run();
 
-  const prompt2 = new MultiSelect({
-    name: "response",
-    message: `What is your desired ${chalk.green(
-      chalk.bold("SCOPE")
-    )} entities?`,
-    initial:
-      (config && config.length > 0 && config.map((item) => item.name)) ||
-      choices.map((item) => item.name),
-    choices,
-    sort: true,
-  });
-
-  const scope = await prompt2.run();
+  switch (type) {
+    case "maximum":
+    case "minimum":
+      scope = choices[type].map((item) => item.name);
+      break;
+    case "custom":
+      const prompt2 = new MultiSelect({
+        name: "response",
+        message: `What is your desired ${chalk.green(
+          chalk.bold("SCOPE")
+        )} entities?`,
+        initial:
+          (config && config.length > 0 && config.map((item) => item.name)) ||
+          choices["maximum"].map((item) => item.name),
+        choices: choices["maximum"],
+        sort: true,
+      });
+      scope = await prompt2.run();
+      type = "maximum";
+      break;
+  }
 
   return {
-    scope: choices
+    scope: choices["maximum"]
       .filter((item) => scope.includes(item.name))
       .map((item) => ({ file: item.file, name: item.name })),
-    scopeType,
+    type,
   };
 };
