3fbf637048b72712db421517b9824fa1b3a687e8
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-11-01 14:26:22 -0300

    mysql config

---
+++ src/config/database/mysql.ts
@@ -0,0 +1,53 @@
+import chalk from "chalk";
+import enquirer from "enquirer";
+import { ConfigDatabaseInterface, QuestionResponse } from "sdz-agent-types";
+
+export default async (config: ConfigDatabaseInterface | undefined) => {
+  const answers: { [key: string]: any } = {};
+
+  answers.host = await enquirer
+    .prompt<QuestionResponse>({
+      type: "input",
+      name: "response",
+      initial: config?.host || "localhost",
+      message: `What is your ${chalk.green(chalk.bold("MySQL"))} host?`,
+    })
+    .then((answer) => answer.response);
+
+  answers.port = await enquirer
+    .prompt<QuestionResponse>({
+      type: "input",
+      name: "response",
+      initial: config?.port || "3306",
+      message: `What is your ${chalk.green(chalk.bold("MySQL"))} port?`,
+    })
+    .then((answer) => answer.response);
+
+  answers.schema = await enquirer
+    .prompt<QuestionResponse>({
+      type: "input",
+      name: "response",
+      initial: config?.schema,
+      message: `What is your ${chalk.green(chalk.bold("MySQL"))} database?`,
+    })
+    .then((answer) => answer.response);
+
+  answers.username = await enquirer
+    .prompt<QuestionResponse>({
+      type: "input",
+      name: "response",
+      initial: config?.username,
+      message: `What is your ${chalk.green(chalk.bold("MySQL"))} username?`,
+    })
+    .then((answer) => answer.response);
+
+  answers.password = await enquirer
+    .prompt<QuestionResponse>({
+      type: "password",
+      name: "response",
+      initial: config?.password,
+      message: `What is your ${chalk.green(chalk.bold("MySQL"))} password?`,
+    })
+    .then((answer) => answer.response);
+  return answers;
+};
