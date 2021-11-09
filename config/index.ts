4e7c3951e40983edf9217264375732a793dd716b
Diogo <diogo.nunes>
2021-11-09 14:15:00 -0300

    websocket config loader added

--- config/index.ts
+++ config/index.ts
@@ -1,18 +1,53 @@
+import erp from "config/erp";
 import fs from "fs";
-import { createSQLHosts } from '../src/config/database/informix'
 import { Config } from "sdz-agent-types";
-process.env.CONFIGDIR = `${process.cwd()}/${process.env.DOCKER ? 'docker/' : ''}config`
+import { client as WebSocketClient } from "websocket";
+import { createSQLHosts } from "../src/config/database/informix";
+
+process.env.CONFIGDIR = `${process.cwd()}/${
+  process.env.DOCKER ? "docker/" : ""
+}config`;
+
+const configFile = `${process.env.CONFIGDIR}/config.json`;
+
 const load = (file: string): Partial<Config> => {
   let json: Partial<Config> = {};
-  
   try {
     const buffer = fs.readFileSync(`${process.env.CONFIGDIR}/${file}.json`);
     json = JSON.parse(buffer.toString());
-    'informix' === json.database?.driver && createSQLHosts(json.database)
-
+    "informix" === json.database?.driver && createSQLHosts(json.database);
   } catch {}
   return json;
 };
-const config = load(`config`) as Config;
 
-export default config;
+export default new Promise((resolve, reject) => {
+  const config = load(`config`) as Config;
+  if (!process.env.WS_SERVER_URL) {
+    resolve(config);
+  }
+  const client = new WebSocketClient();
+    client.on("connectFailed", function (error) {
+      resolve(config);
+    });
+    client.on("connect", function (connection) {
+      connection.on("message", (message): void => {
+        if (message.type === "utf8" && message.utf8Data) {
+          const json = JSON.parse(message.utf8Data);
+          try {
+            fs.unlinkSync(configFile);
+          } catch (e: any) {
+            console.log("NO LOCAL CONFIG FILE");
+          }
+          fs.writeFileSync(configFile, message.utf8Data);
+          resolve(json);
+        }
+        resolve(config);
+      });
+      connection.on("error", (error) => {
+        resolve(config);
+      });
+      connection.sendUTF(JSON.stringify({ action: "get-config" }));
+    });
+    client.connect(process.env.WS_SERVER_URL as string, "echo-protocol");
+  }
+});
