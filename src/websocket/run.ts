f7eef5abcc8089073cca6bccbeeb3b75e02c1310
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-03 11:34:57 -0300

    websocket execute run

--- src/websocket/run.ts
+++ src/websocket/run.ts
@@ -2,7 +2,7 @@ import { Socket } from "socket.io-client";
 
 export default async (socket: Socket, CREDENTIALS: any): Promise<boolean> => {
   return new Promise((resolve) => {
-      require(`../src/bootstrap.ts`);
+      require(`../bootstrap.ts`);
       resolve(true);
   });
 };
