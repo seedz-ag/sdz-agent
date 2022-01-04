7b5ee697108ee57c491bfbabf67e19eda462718b
Diogo Nunes <diogo.nunes>
2022-01-04 19:46:24 -0300

    update changed to async with promise

--- src/websocket/update.ts
+++ src/websocket/update.ts
@@ -1,11 +1,14 @@
 import { exec } from "child_process";
 
-export default async (cb: any): Promise<void> => {
-  exec("git pull", (error, stdout, stderr) => {
-    if (error) {
-      cb(stderr);
-      return;
-    }
-    cb(stdout);
+export default async (cb: any): Promise<boolean> => {
+  return new Promise(async (resolve) => {
+    await exec("git pull", (error, stdout, stderr) => {
+      if (error) {
+        cb(stderr);
+        return;
+      }
+      cb(stdout);
+    });
+    resolve(true);
   });
 };
