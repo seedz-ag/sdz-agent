015cf3d31139077119cc345e0bb2426ed801b2de
Diogo Nunes <diogo.nunes>
2022-01-05 17:45:14 -0300

    modified

--- src/websocket/run.ts
+++ src/websocket/run.ts
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 
 export default async (args: string, cb: any): Promise<boolean> => {
   return new Promise(async (resolve) => {
-    await exec(`./bin/run ${args || ''}`, (error, stdout, stderr) => {
+    await exec(`./bin/run --isWS ${args || ''}`, (error, stdout, stderr) => {
       if (error) {
         cb(stderr);
         return;
