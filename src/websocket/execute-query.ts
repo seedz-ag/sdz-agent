e619fceac7731d146715fbc3db53c3cd37f849bc
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-07 17:05:44 -0300

    fix

--- src/websocket/execute-query.ts
+++ src/websocket/execute-query.ts
@@ -7,6 +7,7 @@ export default async (query: string, cb?: any): Promise<void> => {
 	const config = await require(`${process.cwd()}/config`).default as Config;
 	const database = new Database(config.database);
 	const result = await database.getConnector().execute(query);
+	console.log(result);
 	if (cb) {
 		cb(result);
 		return;
