3d1ee18309202ce331628f0432dc4832b916ed10
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-11 14:36:28 -0300

    execute query

--- src/websocket/execute-query.ts
+++ src/websocket/execute-query.ts
@@ -7,10 +7,10 @@ export default async (query: string, cb?: any): Promise<void> => {
 	const config = await require(`${process.cwd()}/config`).default as Config;
 	const database = new Database(config.database);
 	const result = await database.getConnector().execute(query);
-	console.log(result);
 	if (cb) {
 		cb(result);
 		return;
 	}
 	console.log(result);
+	process.exit(0);
 }
