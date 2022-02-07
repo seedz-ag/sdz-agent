244b9ff09e40cda6fbd4b2c08de821cf003679df
rubens.curvello <rubens.curvello@hotmail.com>
2022-02-07 11:46:03 -0300

    execute query

--- src/websocket/execute-query.ts
+++ src/websocket/execute-query.ts
@@ -3,8 +3,7 @@ import { Config, DatabaseRow } from "sdz-agent-types";
 
 require('dotenv').config();
 
-export default async (query: string): Promise<DatabaseRow[]> => {
-	const config = await require(`${process.cwd()}/config`).default as Config;
+export default async (config: Config, query: string,): Promise<DatabaseRow[]> => {
 	const database = new Database(config.database);
 	const result = await database.getConnector().execute(query);
 	return result;
