5586322d84d12d504e3a00000bb2f36f72515578
Diogo Nunes <diogo.nunes>
2022-03-07 09:46:02 -0300

    updated

--- bin/run.cmd
+++ bin/run.cmd
@@ -1,4 +1,4 @@
 @echo off
 set LD_LIBRARY_PATH=%CD%/node_modules/sdz-agent-database-oracle/instantclient_21_3/ 
 set INFORMIXDIR=%CD%/node_modules/informixdb/installer/onedb-odbc-driver
-node "%~dp0\run" %*
+node "%~dp0\call" %*
