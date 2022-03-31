5887a823028d89bea7e0ba4968444f6fb9d044a5
rubens.curvello <rubens.curvello@hotmail.com>
2022-03-31 16:40:36 -0300

    pr

--- src/open-id/index.ts
+++ src/open-id/index.ts
@@ -1,4 +1,4 @@
 import { OpenIdClient } from './auth';
 require("dotenv").config();
 
-export default new OpenIdClient(`${process.env.ISSUER_URL}`, `${process.env.CLIENT_ID}`, `${process.env.CLIENT_SECRET}`);
+export default new OpenIdClient(`${process.env.ISSUER_URL}`, `${process.env.CLIENT_ID}`, `${process.env.CLIENT_SECRET}`); 
