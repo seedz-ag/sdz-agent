7b8631007e17f399f0d3eb60969e2974f44d3554
Wander Veloso <wander.veloso@seedz.ag>
2022-06-09 17:06:35 -0300

    implements new config option

---
+++ src/config/is-new-config.ts
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+const { Select } = require("enquirer");
+
+export default async () => {
+  const question = new Select({
+    choices: ["yes", "no"],
+    name: "response",
+    message: `New ${chalk.green(chalk.bold("config"))}?`,
+  });
+  const response = await question.run();
+
+  return response === "yes" ? true : false;
+};
