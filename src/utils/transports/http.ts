ac261c536379b116c044a9a8951e566d3b51ef8b
Diogo Nunes <diogo.nunes>
2022-06-14 15:04:47 -0300

    singleton instance changed to runtime

--- src/utils/transports/http.ts
+++ src/utils/transports/http.ts
@@ -1,25 +1,29 @@
-import writeJson from "../write-json";
 import { TransportSeedz } from "sdz-agent-transport";
+import writeJson from "../write-json";
 
-const instance = new TransportSeedz(
-  String(process.env.ISSUER_URL),
-  String(process.env.API_URL)
-);
-
-instance.setUriMap({
-  faturamento: "invoices",
-  faturamentoItem: "invoice-items",
-  estoque: "inventories",
-  item: "items",
-  itemGrupo: "groups",
-  itemBranding: "brands",
-});
+let instance: TransportSeedz;
 
 const transport = (entity: string, payload: any) => {
   writeJson(entity, payload);
   return instance.send(entity, payload);
 };
 
-transport.getInstance = () => instance;
+transport.getInstance = () => {
+  if (!instance) {
+    instance = new TransportSeedz(
+      String(process.env.ISSUER_URL),
+      String(process.env.API_URL)
+    );
+    instance.setUriMap({
+      faturamento: "invoices",
+      faturamentoItem: "invoice-items",
+      estoque: "inventories",
+      item: "items",
+      itemGrupo: "groups",
+      itemBranding: "brands",
+    });
+  }
+  return instance;
+};
 
 export default transport;
