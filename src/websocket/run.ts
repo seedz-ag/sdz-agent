70c7b048538f918c4c70b3b02a0a5411d41dba38
rubens.curvello <rubens.curvello@hotmail.com>
2022-07-26 17:46:48 -0300

    bug fix multiplos comandos agent

--- src/websocket/run.ts
+++ src/websocket/run.ts
@@ -5,8 +5,19 @@ import bootstrap from "../callstack";
 export default async (config: Config | Config[], args: string): Promise<boolean> => {
   let configName = 'default'
   if (args) {
-    args.split("--").forEach(arg => process.argv.push(`--${arg}`.trim()))
+
+    args.split("--").forEach(arg => {    
+      const needle = arg.split('=')[0];
+      const index = process.argv.findIndex(arg => arg.includes(needle))
+      if(index === -1) {
+        process.argv.push(`--${arg}`.trim());
+      } else {
+        process.argv[index] = arg != '' ? `--${arg}`.trim() : '';
+      }
+    });
+
     const argv: {[key: string]: any} = yargs(process.argv).argv;
+
     configName = argv.config ? argv.config : configName;
   }
 
