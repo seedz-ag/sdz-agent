da71b959320a48e63fd4dfee21053e0746b6deaa
rubens.curvello <rubens.curvello@hotmail.com>
2024-08-01 10:39:42 -0300

    transport

--- src/transports/s3.transport.ts
+++ src/transports/s3.transport.ts
@@ -34,7 +34,6 @@ export default class S3Transport implements ITransport {
   }
 
   private upload(resource: string, chunk: unknown[]) {
-    console.log(resource);
     return this.s3.putObject({
       Body: JSON.stringify(chunk),
       Bucket: this.environmentService.get("AMAZON_S3_RAW_BUCKET"),
